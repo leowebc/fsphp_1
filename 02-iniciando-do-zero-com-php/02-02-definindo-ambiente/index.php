6443c1a1105122e8515e06d01cdba2cd04abaebf
leozulu <leozulu@me.br>
2024-03-12 10:03:18 -0300

    novo teste 001

--- 02-iniciando-do-zero-com-php/02-02-definindo-ambiente/index.php
+++ 02-iniciando-do-zero-com-php/02-02-definindo-ambiente/index.php
@@ -2,6 +2,11 @@
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("02.02- Definindo ambiente ");
 
+
+testenado commite
+mandando para o outro 
+
+
 /**
  * [ echo ] https://php.net/manual/pt_BR/function.echo.php
  */
