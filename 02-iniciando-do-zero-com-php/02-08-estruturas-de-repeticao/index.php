ac748cb7b2fcfdf2c7df192a95a1938590bac02f
leozulu <leozulu@me.br>
2024-03-12 12:40:50 -0300

    mais 2 exemplos

--- 02-iniciando-do-zero-com-php/02-08-estruturas-de-repeticao/index.php
+++ 02-iniciando-do-zero-com-php/02-08-estruturas-de-repeticao/index.php
@@ -8,11 +8,32 @@ fullStackPHPClassName("02.08 - Estruturas de repetição");
  */
 fullStackPHPClassSession("while, do while", __LINE__);
 
+$looping = 1;
+$while = [];
+
+while ( $looping <= 5 ){
+    $while[] = $looping;
+    $looping++;
+}
+var_dump($while);
+
+$looping = 5;
+$while = [];
+
+do {
+    $while[] = $looping;
+    $looping--;
+} while ($looping >= 1);
+var_dump($while);
 
 /*
  * [ for ] https://php.net/manual/pt_BR/control-structures.for.php
  */
 fullStackPHPClassSession("for", __LINE__);
+
+for($i= 1; $i <= 10; $i++){
+    echo "<p>{$i}</p>";
+}
 
 
 /**
@@ -21,8 +42,28 @@ fullStackPHPClassSession("for", __LINE__);
  */
 fullStackPHPClassSession("break, continue", __LINE__);
 
+for($c= 1; $c <= 10; $c++){
+    if($c % 2 == 1){
+        continue;
+    }
+
+    if($c >= 10){
+        break;
+    }
+
+    echo "<p>Pulou + :: {$c}</p>";
+
+}
+
 
 /**
  * [ foreach ] https://php.net/manual/pt_BR/control-structures.foreach.php
  */
 fullStackPHPClassSession("foreach", __LINE__);
+
+$arr = [];
+for($ar = 0; $ar <= 10; $ar++){
+   $array[] = $ar;
+}
+
+var_dump($array);
--- 02-iniciando-do-zero-com-php/02-09-closures-e-generators/index.php
+++ 02-iniciando-do-zero-com-php/02-09-closures-e-generators/index.php
@@ -7,8 +7,61 @@ fullStackPHPClassName("02.09 - Closures e generators");
  */
 fullStackPHPClassSession("closures", __LINE__);
 
+$myAge = function ($year){
+    $age = date("Y") - $year;
+    return "<p> Você tem {$age} anos</p>";
+};
+
+$priceRrl = function ($price){
+    return number_format($price, 2, ",", ".");
+};
+
+echo $myAge(1981);
+
+echo "<p>O Pojeto custa R$ {$priceRrl(3600)}. vamos fachar</p>";
+
+$myCart = [];
+$myCart["totalPrice"] = 0;
+$cardAdd = function ($item, $qtd, $price ) use (&$myCart){
+    $myCart[$item] = $qtd * $price;
+    $myCart["totalPrice"] += $myCart[$item];
+};
+
+$cardAdd("HTML5", 1, 497);
+$cardAdd("jquery", 2, 497);
+
+ var_dump($myCart);
+
 
 /*
  * [ generators ] https://php.net/manual/pt_BR/language.generators.overview.php
  */
 fullStackPHPClassSession("generators", __LINE__);
+
+
+
+$iterator = 400;
+
+function generatorDate($days)
+{
+    for ($day = 1; $day < $days; $day++){
+        yield date("d/m/Y", strtotime("+{$day}days"));
+    }
+}
+
+echo"<div style='text-align:center'>";
+foreach (generatorDate($iterator) as $date) {
+    echo "<small class='tag'>{$date}</small>" . PHP_EOL;
+}
+
+
+
+
+
+
+
+
+
+
+
+
